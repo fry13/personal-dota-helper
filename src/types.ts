6f239c5362ce626d39ac9736134ff3dfffa2c9fe
Alexey Feklyunin <ninetailed1995@gmail.com>
2023-02-28 09:26:38 +0400

    feat: add new user type

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,32 @@
-export type User = {
+export type UserFromSearch = {
   account_id: number,
   avatarfull: string,
   personaname: string,
   last_match_time: string,
   similarity: 0
 };
+
+export type User = {
+  account_id?: number,
+  personaname?: string,
+  avatarfull?: string,
+  profileurl?: string,
+  rank_tier?: number,
+  all_win?: number,
+  all_lose?: number,
+  all_win_14days?: number,
+  all_lose_14days?: number,
+  all_win_30days?: number,
+  all_lose_30days?: number,
+  all_win_60days?: number,
+  all_lose_60days?: number,
+  ranked_win?: number,
+  ranked_lose?: number,
+  ranked_win_14days?: number,
+  ranked_lose_14days?: number,
+  ranked_win_30days?: number,
+  ranked_lose_30days?: number,
+  ranked_win_60days?: number,
+  ranked_lose_60days?: number,
+  
+}
